06246f0074869f01af18362eaa5eee5254c8b2ec
appstronautsin-in <appstronauts.in@gmail.com>
2026-01-03 18:10:14 +0530

    Updated CreateNews.tsx

--- src/components/CreateNews.tsx
+++ src/components/CreateNews.tsx
@@ -912,7 +912,7 @@ const CreateNews: React.FC<CreateNewsProps> = ({ onClose, showAlert }) => {
                 </option>
               ))}
             </select>
-            <p className="mt-1 text-sm text-gray-500">This Secondary category becomes primary category.</p>
+            <p className="mt-1 text-sm text-gray-500">This Secondary category becomes primary category after 24 hours.</p>
           </div>
 
           <div>
